6c8ae0cf99763732761f5490f5c1d82aae53240d
AndreMotta25 <andremotta16041998@gmail.com>
2023-02-15 14:42:40 -0300

    feat: Distributing the provider to the application

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import ToastProvider from './context/Toast';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ToastProvider>
+      <App />
+    </ToastProvider>
   </React.StrictMode>
 );
 
