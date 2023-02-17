19d5f7a182307a09de687dd5dcec18ca29902f76
AndreMotta25 <andremotta16041998@gmail.com>
2023-02-17 00:22:12 -0300

    feat: placing cards in the menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import * as S from './styles'
 import Search from '../Search'
+import { ICard } from '../../pages/Home';
+import Card from '../Card';
 
 
 interface IModalController {
@@ -14,15 +16,32 @@ interface IMenu {
   research: React.Dispatch<React.SetStateAction<string>>;
   search: string;
   modalController: IModalController;
+  cards: ICard[],
+  fillModal: (card:ICard) => void;
+  clearModal: () => void;
+  activeCard: ICard | null;
+  deleteCard: (id:string) => void;
 }
-const Menu = ({research, search, modalController}:IMenu) => {
+
+const Menu = ({research, search, modalController, cards, fillModal, clearModal, activeCard, deleteCard}:IMenu) => {
+  const activeModal = () => {
+    clearModal();
+    modalController.activeElement();
+  }
+
   return (
     <S.Container>
         <Search value={search} onChange={({target}) => research(target.value)}/>
-
-        <button onClick={modalController.activeElement} >Ativar Modal</button>
+        <S.ContainerCards>
+          {
+            cards.map((card) => (
+              <Card card={card} key={card.id} fillModal={fillModal} isActive={card.id === activeCard?.id} deleteCard={deleteCard}/>
+            ))
+          }
+        </S.ContainerCards>
+        <S.Button onClick={activeModal}>Criar Card</S.Button>
     </S.Container>
   )
 }
 
-export default Menu
+export default React.memo(Menu)
--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -1,7 +1,33 @@
 import styled from 'styled-components';
+import { ButtonStyled } from '../Button/styles';
 
 export const Container = styled.div`
   background-color: transparent;
   padding:3rem;
   width: 30%;
+  display: flex;
+  flex-direction: column;
+  position: relative;
 `;
+
+export const ContainerCards = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  overflow: auto;
+  height: 50vh;
+  width: 100%;
+  
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`
+
+export const Button = styled(ButtonStyled)`
+  position: absolute;
+  bottom:3rem;
+  padding: 0.8rem;
+  color: #fff;
+  margin: 0 auto;
+  align-self: center;
+`
