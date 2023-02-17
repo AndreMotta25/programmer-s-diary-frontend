c6fc4f6eb6f5fc78a7a2270f31c021ea1cfd83d8
AndreMotta25 <andremotta16041998@gmail.com>
2023-02-17 00:19:48 -0300

    feat: creating the component card

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,55 @@
+import React, { useState , MouseEvent, useRef} from 'react'
+import { AiFillDelete } from 'react-icons/ai';
+import useShortText, { IShortText } from '../../hooks/useShortText';
+import { ICard } from '../../pages/Home'
+import * as S from './styles' 
+
+interface IProps {
+    card: ICard
+    fillModal: (card:ICard) => void;
+    isActive: boolean;
+    deleteCard: (id:string) => void;
+}
+
+const Card = ({card, fillModal, isActive, deleteCard}:IProps) => {
+  const descriptionShort: IShortText = {text:card.description, len:50}
+
+  const [description, handleClickDescription] = useShortText(descriptionShort);
+  const [active, setActive] = useState(false);
+
+  const descriptionRef = useRef<HTMLParagraphElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null) 
+
+
+  const handleOpeningModal = (e: MouseEvent) => {
+    if(e.target !== descriptionRef.current && !containerRef.current?.contains(e.target as HTMLElement)) {
+      fillModal(card)
+    }
+    console.log(e)
+  }
+  
+  const handleDescription = () =>  {
+    handleClickDescription();
+    setActive(!active);
+  }
+
+  return (
+    <S.Container  onClick={handleOpeningModal} extendCard={active} isActive={isActive}>
+        <S.Name>{card.name}</S.Name>
+        <S.Description ref={descriptionRef} onClick={handleDescription}>
+          {description}
+        </S.Description>
+        <S.Extras ref={containerRef}>
+          <S.TypeLanguage>
+            {card.language}
+          </S.TypeLanguage>
+          <S.Button  onClick={() => {deleteCard(card.id)}}>
+            <AiFillDelete size={'100%'}/>
+          </S.Button>
+        </S.Extras>
+        
+    </S.Container>
+  )
+}
+
+export default React.memo(Card)
---
+++ src/components/Card/styles.tsx
@@ -0,0 +1,71 @@
+import styled from 'styled-components';
+
+
+interface IProps {
+  extendCard:boolean;
+  isActive?: boolean;
+}
+
+export const Container = styled.div`
+  width: 100%;
+  border: 0.2rem solid ${({isActive}) => isActive? '#fff' :'#282828'} ;
+  border-radius: 1rem;
+  padding: 0.8rem;
+  min-height: ${({extendCard}:IProps) => (extendCard && 'auto')};
+  cursor: pointer;
+  flex: 0 0 13.2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+`;
+export const Name = styled.h3`
+    font-size: 1.8rem;
+    color: #fff;
+    font-family: "Source Code Pro", monospace;
+`
+export const Description = styled.p`
+    font-size: 1.4rem;
+    color: #fff;
+    font-family: "Source Code Pro", monospace;
+    margin-top: 10px;
+    margin-bottom: 20px;
+
+    &:hover {
+      color: #282828;
+    }
+`
+export const TypeLanguage = styled.span`
+  background-color: #282828;
+  padding: 0.5rem;
+  border-radius: 0.5rem;
+  width: fit-content;
+  color: #fff;
+  font-family: "Source Code Pro", monospace;
+
+  &:hover {
+      color: #46FC42;
+  }
+`
+
+export const Extras = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+`
+export const Button = styled.button`
+  width: 40px;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;  
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  position: relative;
+
+  &:hover {
+    color: #dd2727;
+  }
+`
