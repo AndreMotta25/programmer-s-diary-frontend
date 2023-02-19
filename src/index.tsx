0874fcb8aa814e85582478f5fbd8d7a8a4b22e6d
AndreMotta25 <andremotta16041998@gmail.com>
2023-02-19 18:56:37 -0300

    feat: using the user's provider

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import ToastProvider from './context/Toast';
+import UserProvider from './context/User';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -9,7 +10,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <ToastProvider>
-      <App />
+      <UserProvider>
+        <App />
+      </UserProvider>
     </ToastProvider>
   </React.StrictMode>
 );
