8c7850f4f87d6a47b785a42fc763c06e0da868c8
AndreMotta25 <andremotta16041998@gmail.com>
2023-02-19 18:54:56 -0300

    feat: starting to make use of the user context with information that comes from the api

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -2,22 +2,24 @@ import React, { useState } from 'react'
 import * as S from "./styles"
 import lotus from './lotus.jpg'
 import { Link } from 'react-router-dom';
+import { useUserContext } from '../../hooks/useUserContext'
 
 interface IProps {
   active: boolean;
   activate: React.Dispatch<React.SetStateAction<boolean>>;
 }
 const Profile = ({active, activate}:IProps ) => {
+  const {user} = useUserContext()
   
   const handleClickLogout = () => {
     console.log('fazendo logout');
   }
-
+  console.log(user)
   return (
     <>
     <S.ContainerAvatar active={active}>
         <S.ContainerPhoto onClick={() => {activate(active => !active)}}>
-            <img src={lotus} alt="" />
+            <img src={`http://localhost:3333/avatar/${user?.avatar}`} alt="" />
         </S.ContainerPhoto>
     <S.ActionsList>
         <S.ActionItem>
