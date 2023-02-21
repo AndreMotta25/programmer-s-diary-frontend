182f783001c14002d21db6a325124e3fbd02f8e7
AndreMotta25 <andremotta16041998@gmail.com>
2023-02-21 15:46:25 -0300

    :sparkles: feat: making the logout button functional

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import * as S from "./styles"
-import lotus from './lotus.jpg'
 import { Link } from 'react-router-dom';
 import { useUserContext } from '../../hooks/useUserContext'
 
@@ -9,26 +8,21 @@ interface IProps {
   activate: React.Dispatch<React.SetStateAction<boolean>>;
 }
 const Profile = ({active, activate}:IProps ) => {
-  const {user} = useUserContext()
-  
-  const handleClickLogout = () => {
-    console.log('fazendo logout');
-  }
-  console.log(user)
+  const {user, logout} = useUserContext()
+
   return (
     <>
-    <S.ContainerAvatar active={active}>
-        <S.ContainerPhoto onClick={() => {activate(active => !active)}}>
-            {user && <img src={`http://localhost:3333/avatar/${user?.avatar}`} alt="" />}
-        </S.ContainerPhoto>
-    <S.ActionsList>
-        <S.ActionItem>
-          <Link to='/usuario/editar'> Alterar Dados </Link>
-        </S.ActionItem>
-        <S.ActionItem onClick={handleClickLogout}>Logout</S.ActionItem>
-    </S.ActionsList>
-    </S.ContainerAvatar>
-    
+      <S.ContainerAvatar active={active}>
+          <S.ContainerPhoto onClick={() => {activate(active => !active)}}>
+              {user && <img src={`http://localhost:3333/avatar/${user?.avatar}`} alt="" />}
+          </S.ContainerPhoto>
+      <S.ActionsList>
+          <S.ActionItem>
+            <Link to='/usuario/editar'> Alterar Dados </Link>
+          </S.ActionItem>
+          <S.ActionItem onClick={logout}>Logout</S.ActionItem>
+      </S.ActionsList>
+      </S.ContainerAvatar>
     </>
   )
 }
