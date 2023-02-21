af7203632bba580baaa8da32247ba77d48eb1007
AndreMotta25 <andremotta16041998@gmail.com>
2023-02-21 01:33:06 -0300

    :sparkles: feat: authentication api

---
+++ src/api/AuthenticationApi.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from "./shared/HttpClient";
+
+interface IAuthenticate {
+    identification:string;
+    password:string;
+}
+export interface IAuthenticateResponse {
+    user: {
+        email:string;
+        id:string;
+        avatar:string;
+    }
+    token:string;
+}
+
+class AuthenticationApi extends HttpClient{
+    private static apiInstance: AuthenticationApi;
+
+    private constructor() {
+        super('http://localhost:3333/sessions')
+    }
+    
+    public static getInstance():AuthenticationApi {
+        if(!AuthenticationApi.apiInstance)
+           return new AuthenticationApi();
+        return AuthenticationApi.apiInstance;   
+    }
+    async authenticateUser({identification,password}:IAuthenticate) {
+        return await this.instance.post<IAuthenticateResponse>('/', {identification,password});
+    }
+
+    async logoutUser(){
+        await this.instance.patch('/logout')
+    }
+    async validateToken(token:string) {
+        return (await this.instance.post<{message:string}>(`/validate-token/${token}`)).message;
+    }
+}
+export {AuthenticationApi}
