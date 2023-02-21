9d220276afa2fd3b174f76a49737bb5fcd9f4f49
AndreMotta25 <andremotta16041998@gmail.com>
2023-02-21 01:30:46 -0300

    :sparkles: feat: validating the token

--- src/context/User/index.tsx
+++ src/context/User/index.tsx
@@ -28,59 +28,67 @@ export const UserContext = createContext({} as IUserContext);
 
 const UserProvider = ({children}:IProps) => {
   const [user, setUser] = useState<IUser | null>(null);
-  /*
-        Depois mexer aqui para fazer um if e verificar se o token está valido para só assim atribui-lo aqui.
-        Se o if ficar aqui, não vou precisar de usar em outros lugares da aplicação
-  */ 
-  const [token, setToken] = useState<string | null>(() => localStorage.getItem('token'))
+
+  const [token, setToken] = useState<Promise<string | null>>(async () => {
+    const token = localStorage.getItem('token');
+    try{
+      if(token)
+        await userAuthenticate.validateToken(token);
+      return token;
+    }
+    catch{
+      localStorage.removeItem('token')
+      return null
+    }
+    
+  });
+
+
   const navigate = useNavigate(); 
 
   const sign = async ({identification, password}:IRequestUser) => {
-        const credentias = await userAuthenticate.AuthenticateUser({identification,password});
+        const credentias = await userAuthenticate.authenticateUser({identification,password});
 
-        // userAPI.setAuthorization(credentias.token);
         setTokenLocalStorage(credentias.token);
         setUser(credentias.user);
-        setToken(credentias.token)
+        setToken(new Promise(resolve => {resolve(credentias.token)}) )
   } 
 
-  const setAuthorizations = () => {
-    if(token){
-        userAPI.setAuthorization(token);
-        userAuthenticate.setAuthorization(token);
+  const setAuthorizations = async () => {
+    const tokenValid = await token;
+    if(tokenValid){
+        userAPI.setAuthorization(tokenValid);
+        userAuthenticate.setAuthorization(tokenValid);
     }
   }
 
-  console.log(userAPI)
-  /*
-    Tem que excluir o token do localStorage e acessar a api e levar para pagina de login
-  */   
   const logout = async () => {
-    // if(token){
-        // userAuthenticate.setAuthorization(token);
-    await userAuthenticate.LogoutUser();
-    // }
+    await userAuthenticate.logoutUser();
     localStorage.removeItem('token');
   }
 
   const setTokenLocalStorage = (token:string) => localStorage.setItem('token', token);
-  
+
   useEffect(() => {
     setAuthorizations();   
   // eslint-disable-next-line react-hooks/exhaustive-deps
   },[token])
 
   useEffect(() => {
-    if(token && userAPI.hasAuthorization()) {
-        // userAPI.setAuthorization(token);
-        userAPI.getUser().then(user => {
-            setUser(user);
-            navigate('/')
-        })
-    } 
+    const reloadPage = async () => {
+      if(await token && userAPI.hasAuthorization()) {
+          userAPI.getUser().then(user => {
+              setUser(user);
+              navigate('/')
+          })
+      } 
+    }
+    reloadPage()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   },[])
 
-  
+  console.log(userAPI)
+
 
   return (
     <UserContext.Provider value={{
