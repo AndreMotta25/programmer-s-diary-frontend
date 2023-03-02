7cf5b471eded674e54e108f47ddb6fd5646dd1a8
AndreMotta25 <andremotta16041998@gmail.com>
2023-03-02 16:18:39 -0300

    :sparkles: feat: reset-password routes if forgot-password

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import ToastFy from './components/Toastfy';
 import UserProvider from './context/User';
 import ConfirmEmail from './pages/ConfirmEmail';
+import ForgetPassword from './pages/ForgetPassword';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Redirect from './pages/Redirect';
 import Register from './pages/Register';
+import ResetPassword from './pages/ResetPassword';
 import Update from './pages/Update';
 import GlobalStyle from './styles/globalStyles';
 import theme from "./styles/theme"
@@ -19,11 +23,13 @@ function App() {
         <BrowserRouter>
           <UserProvider>
             <Routes>
-              <Route path='/' element={<Home/>}/>
+              <Route path='/' element={<Redirect><Home/></Redirect>}/>
               <Route path='/login' element={<Login/>}/>
-              <Route path='/usuario/editar/*' element={<Update/>}/>
+              <Route path='/usuario/editar/*' element={<Redirect><Update/></Redirect>}/>
               <Route path='/registrar' element={<Register/>}/>
               <Route path='/confirmar-email/:token' element={<ConfirmEmail/>}/>
+              <Route path='/recuperar-senha' element={<ForgetPassword/>}/>
+              <Route path='/reset-senha/:token' element={<ResetPassword/>}/>
             </Routes>
           </UserProvider>
         </BrowserRouter>
