8bbeed72caa0dc34c5b31e78622c0dab9facb530
AndreMotta25 <andremotta16041998@gmail.com>
2023-03-02 15:57:19 -0300

    :sparkles: feat: password reset page

---
+++ src/pages/ResetPassword/index.tsx
@@ -0,0 +1,67 @@
+import { useFormik } from 'formik';
+import React, { useEffect } from 'react'
+import { useNavigate, useParams } from 'react-router-dom';
+import ContainerGeneric from '../../components/ContainerGeneric'
+import Input from '../../components/Input';
+import { Button } from '../Login/styles';
+import {passwordAPI} from '../../api/'
+import useHandlerError from '../../hooks/useHandlerError';
+import { resetPasswordSchema } from '../../validations/resetPasswordSchema';
+import { useToastContext } from '../../hooks/useToast';
+
+const ResetPassword = () => {
+  
+  const navigate = useNavigate();
+
+  const {errors,handleError} = useHandlerError();
+  const {token} = useParams();
+  const {toast} = useToastContext();  
+
+  const handleSubmit = async () => {
+    if(token) {
+        try{
+            await passwordAPI.resetPassword({token:token, new_password:formik.values.password});
+            toast('Senha Atualizada',{status:'success',autoCloseIn:2000});
+            setTimeout(() => {
+                navigate('/login')
+            },2000);
+            
+        }
+        catch(e) {
+            handleError(e);
+        }
+    }
+  } 
+  
+  const formik = useFormik({
+    initialValues: {
+        password:'',
+        same_password:''
+    },
+    onSubmit:handleSubmit,
+    validationSchema:resetPasswordSchema
+  });
+
+  useEffect(() => {
+    if(!token) {
+        navigate('/login');
+    }
+  },[token]) 
+
+  useEffect(() => {
+    formik.setErrors({...formik.errors,...errors })
+  },[errors])
+
+  return (
+    <ContainerGeneric>
+        <form onSubmit={formik.handleSubmit}>
+            <Input onBlurFormik={formik.handleBlur} type='password' onChange={formik.handleChange} value={formik.values.password} label='Senha' id='password' name='password' variant='solid' error={(formik.touched.password && formik.errors.password) || ''}/>
+            <Input onBlurFormik={formik.handleBlur} type='password' onChange={formik.handleChange} value={formik.values.same_password} label='Repita a senha' id='same_password' name='same_password'error={(formik.touched.same_password && formik.errors.same_password) || ''} variant='solid'/>
+            <Button type='submit'>Resetar Senha</Button>
+        </form>
+    </ContainerGeneric>
+    
+  )
+}
+
+export default ResetPassword;
