8133dadf0084de8a6c11cfc329187783ce2ebda8
AndreMotta25 <andremotta16041998@gmail.com>
2023-03-02 16:04:22 -0300

    :sparkles: feat: instantiating some api's

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,3 +1,7 @@
+import { AuthenticationApi } from "./AuthenticationApi";
+import {PasswordAPI} from "./PasswordApi";
 import { UserAPI } from "./UserApi";
 
 export const userAPI = UserAPI.getInstance();
+export const userAuthenticate = AuthenticationApi.getInstance();
+export const passwordAPI = PasswordAPI.getInstance();
