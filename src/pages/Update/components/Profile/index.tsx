e34ad049c78fd235a080a36c30459c67a0387620
AndreMotta25 <andremotta16041998@gmail.com>
2023-03-19 00:47:34 -0300

    :sparkles: feat: loading the profile

--- src/pages/Update/components/Profile/index.tsx
+++ src/pages/Update/components/Profile/index.tsx
@@ -1,5 +1,5 @@
 import { useFormik } from 'formik';
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { userAPI } from '../../../../api';
 import Input from '../../../../components/Input';
 import { useToastContext } from '../../../../hooks/useToast';
@@ -7,11 +7,13 @@ import { useUserContext } from '../../../../hooks/useUserContext';
 import { Button, ContainerUpdate } from '../../styles';
 import useHandlerError from '../../../../hooks/useHandlerError';
 import { Helmet } from 'react-helmet';
+import Loading from '../../../../components/Loading';
 
 const Profile = () => {
   const {user,valid} = useUserContext();
   const {toast} = useToastContext();
   const {errors, handleError} = useHandlerError();
+  const [loading, setLoading] = useState(true);
 
   const handleSubmit = async () => {
     try {
@@ -38,17 +40,17 @@ const Profile = () => {
     formik.setErrors({...formik.errors, ...errors})
   },[errors])
 
-  // isso é para evitar de fazer um get na api
   useEffect(() => {
     const getUser = async () => {
       const isValid = await valid();
       
       if(isValid && userAPI.hasAuthorization()){
         const userResult = await userAPI.getUser();
+        setLoading(false); 
         formik.setValues({
           email: userResult.email,
           username: userResult.username
-        })  
+        })
       }
     }
     getUser();
@@ -60,13 +62,15 @@ const Profile = () => {
       <Helmet>
         <title>Diário do Programador - Perfil</title>
       </Helmet>
-      <form onSubmit={formik.handleSubmit}>
+      {(loading && <Loading/>) ||  
+       <form onSubmit={formik.handleSubmit}>
         <ContainerUpdate>
           <Input type='email' variant='solid' label='Email' value={formik.values.email} name='email' id='email' onChange={formik.handleChange} error={(formik.touched.email && formik.errors.email) || ''} />
           <Input type='text' variant='solid' label='Username' value={formik.values.username} name='username' id='username' onChange={formik.handleChange} error={(formik.touched.username && formik.errors.username) || ''}/>
         </ContainerUpdate>
         <Button type='submit'>Atualizar usuario</Button>
-      </form>
+      </form>}
+      
     </>
   )
 }
